6f9c44f7170f7991dbbdb93fc8297039fc8e60a7
Yehuda Deutsch <yeh@uda.co.il>
2016-11-11 15:34:08 +0200

    Initial work based on e-orz's GH PR
    Summary:
    Based on e-orz's initial work in https://github.com/e-orz/ravkav_linux
    I started to work on Ubuntu's port
    There is still an issue with the server rejecting the jar with version 0.1.4
    The current working version is 0.2.0 and for that we need the windows version
    If we can find a way to extract that without `wine` that will help make this simpler
    
    Test Plan:
    * Run `./Ubuntu/installRavKav.sh`
    * Enter 'https://ravkavonline.co.il/he/'
    * Follow the steps
    * Authorize the site to open the external app (registered with scheme `ravkav`)
    
    Differential Revision: https://hamakor.phab.npo.org.il/D1

---
+++ Ubuntu/installRavKav.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+pkgname=ravkav-bin
+pkgver=0.1.4
+
+sudo cp ../Common/ravkav "/usr/bin/ravkav"
+sudo cp ../Common/RavKavOnline.desktop "/usr/share/applications/RavKavOnline.desktop"
+sudo update-desktop-database
+wget "https://ravkavonline.co.il/releases/osx/ravkav-online-osx-$pkgver.zip" -O "$pkgname-$pkgver.zip"
+unzip -j "$pkgname-$pkgver.zip" 'RavKavOnline/RavKavOnline 64.app/Contents/Java/RavKavOnline.jar' -d ./
+sudo mkdir -p /usr/share/java/ravkav && sudo cp RavKavOnline.jar /usr/share/java/ravkav/RavKavOnline.jar
